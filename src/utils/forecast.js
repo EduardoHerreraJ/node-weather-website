df934bae47e1e05a9e81d5b48f0bf36bc802af73
Eduardo Herrera <33185685+EduardoHerreraJ@users.noreply.github.com>
2021-07-12 14:21:52 -0500

    Solve forecast error and add wind speed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,8 +1,10 @@
 const request = require('request')
 
-const forecast = (longitud, latitud,callback) =>{
+const forecast = (latitude, longitude,callback) =>{
+    console.log(latitude,longitude)
     
-    const url = 'http://api.weatherstack.com/current?access_key=e5fbaf23890d0931c65b2739976d1a5e&query='+ encodeURIComponent(latitud) +','+ encodeURIComponent(longitud) +'units=m'
+    const url = 'http://api.weatherstack.com/current?access_key=e5fbaf23890d0931c65b2739976d1a5e&query='+ encodeURIComponent(latitude) +','+ encodeURIComponent(longitude)
+    console.log(url)
     
     request({url,json : true}, (error, {body}={}) => {
         
@@ -12,7 +14,7 @@ const forecast = (longitud, latitud,callback) =>{
             callback('Unable to find location', undefined)
         }else{
             callback(undefined,
-                 body.current.weather_descriptions[0]+'it is ' + body.current.temperature+ ' degress out, but it feels like '+body.current.feelslike+ ' degrees' ,
+                 body.current.weather_descriptions[0]+' it is ' + body.current.temperature+ ' degress out, but it feels like '+body.current.feelslike+ ' degrees, and the wind speed is of '+body.current.wind_speed ,
             )
         }
 
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const geocode = (address,callback) =>{
     
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1IjoiZWR1YXJkb2hqIiwiYSI6ImNrbndsNTZ5ZzBzd3Uyb3BqODV6Y2luYnIifQ.gEYbcIXq1zW13AibR1l1cw&limit=1'
+    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1IjoiZWR1YXJkb2hqIiwiYSI6ImNrbndsNTZ5ZzBzd3Uyb3BqODV6Y2luYnIifQ.gEYbcIXq1zW13AibR1l1cw&limit=1'
     
     request({url,json : true}, (error, {body}={}) => {
         
